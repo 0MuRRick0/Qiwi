770533492aee9d55eefb708695ec55736d02f480
MuRRick <mr.murad-mirzali@yandex.ru>
2025-07-26 19:50:25 +0300

    removed /logout endpoint from user api. removed token blacklisting.

--- user_service/accounts/urls.py
+++ user_service/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import RegisterView, LoginView, TokenRefreshView, LogoutView, MeView
+from .views import RegisterView, LoginView, TokenRefreshView,  MeView
+# from .views import LogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', MeView.as_view(), name='me'),
 ]
--- user_service/accounts/views.py
+++ user_service/accounts/views.py
@@ -40,19 +40,19 @@ class TokenRefreshView(APIView):
             return Response({'error': 'Invalid refresh token'}, status=status.HTTP_400_BAD_REQUEST)
         
 
-class LogoutView(APIView):
-    def post(self, request):
-        refresh_token = request.data.get('refresh')
+# class LogoutView(APIView):
+#     def post(self, request):
+#         refresh_token = request.data.get('refresh')
 
-        if not refresh_token:
-            return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
+#         if not refresh_token:
+#             return Response({'error': 'Refresh token is required'}, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response({'message': 'Successfully logged out'}, status=status.HTTP_200_OK)
-        except TokenError:
-            return Response({'error': 'Invalid or expired refresh token'}, status=status.HTTP_400_BAD_REQUEST)
+#         try:
+#             token = RefreshToken(refresh_token)
+#             token.blacklist()
+#             return Response({'message': 'Successfully logged out'}, status=status.HTTP_200_OK)
+#         except TokenError:
+#             return Response({'error': 'Invalid or expired refresh token'}, status=status.HTTP_400_BAD_REQUEST)
         
 User = get_user_model()
 
