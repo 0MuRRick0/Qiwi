26ad4eb8e63f0ef9f0de1ec6826b9508ea5eaf0d
MuRRick <mr.murad-mirzali@yandex.ru>
2025-08-04 21:17:28 +0300

    Sync period of mirror server can now be set as environment variable

--- ftp-mirror-proxy/periodic_sync.sh
+++ ftp-mirror-proxy/periodic_sync.sh
@@ -1,6 +1,18 @@
-#!bin/sh
+#!/bin/sh
+
+SYNC_INTERVAL=${SYNC_INTERVAL:-300}
+
+if ! echo "$SYNC_INTERVAL" | grep -E '^[0-9]+$' > /dev/null || [ "$SYNC_INTERVAL" -le 0 ]; then
+    echo "Ошибка: SYNC_INTERVAL должна быть положительным целым числом. Используется значение по умолчанию 300." >&2
+    SYNC_INTERVAL=300
+fi
+
+echo "Запуск периодической синхронизации с интервалом ${SYNC_INTERVAL} секунд" >&2
 
 while true; do
     /usr/local/bin/sync.sh
-    sleep 60
+    if [ $? -ne 0 ]; then
+        echo "Предупреждение: sync.sh завершился с ошибкой" >&2
+    fi
+    sleep "$SYNC_INTERVAL"
 done
